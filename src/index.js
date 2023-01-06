d7539aeb829187717fd3585b6ee6e30db956cbcc
Max Sidorenko <fivemoshr@gmail.com>
2023-01-06 13:38:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { GlobalStyle } from 'BaseStyles/GlobalStyles';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
         <App />
         <GlobalStyle />
       </BrowserRouter>
